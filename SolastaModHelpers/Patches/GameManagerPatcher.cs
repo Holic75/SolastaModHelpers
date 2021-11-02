db47ebf4a48ee4e082b86630c82fd90335bf8af6
Holic75 <holic.denis@gmail.com>
2021-11-02 19:15:02 +0100

    Update GameManagerPatcher.cs

--- SolastaModHelpers/Patches/GameManagerPatcher.cs
+++ SolastaModHelpers/Patches/GameManagerPatcher.cs
@@ -6,19 +6,6 @@ namespace SolastaModHelpers.Patches
 {
     class GameManagerPatcher
     {
-        [HarmonyPatch(typeof(GamingPlatformManager), "UpdateAvailableDlc")]
-        internal static class GamingPlatformManager_UpdateAvailableDlc_Patch
-        {
-            internal static void Postfix(HashSet<GamingPlatformDefinitions.ContentPack> ___unlockedContentPacks)
-            {
-
-                ___unlockedContentPacks.Add(GamingPlatformDefinitions.ContentPack.BackerItems);
-                ___unlockedContentPacks.Add(GamingPlatformDefinitions.ContentPack.DigitalBackerContent);
-                ___unlockedContentPacks.Add(GamingPlatformDefinitions.ContentPack.LoadedDice);
-                ___unlockedContentPacks.Add(GamingPlatformDefinitions.ContentPack.PrimalCalling);
-            }
-        }
-
         [HarmonyPatch(typeof(GameManager), "BindPostDatabase")]
         internal static class GameManager_BindPostDatabase_Patch
         {
