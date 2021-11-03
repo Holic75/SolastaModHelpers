b013336da281e2b537ad100439f51949c5828417
Holic75 <holic.denis@gmail.com>
2021-11-03 19:32:10 +0100

    made translation loading more generic

--- SolastaModHelpers/Main.cs
+++ SolastaModHelpers/Main.cs
@@ -20,42 +20,12 @@ namespace SolastaModHelpers
         internal static void Error(string msg) => Logger?.Error(msg);
         internal static UnityModManager.ModEntry.ModLogger Logger { get; private set; }
 
-        internal static void LoadTranslations()
-        {
-            DirectoryInfo directoryInfo = new DirectoryInfo($@"{UnityModManager.modsPath}/SolastaModHelpers");
-            FileInfo[] files = directoryInfo.GetFiles($"Translations-??.txt");
-
-            foreach (var file in files)
-            {
-                var filename = $@"{UnityModManager.modsPath}/SolastaModHelpers/{file.Name}";
-                var code = file.Name.Substring(13, 2);
-                var languageSourceData = LocalizationManager.Sources[0];
-                var languageIndex = languageSourceData.GetLanguageIndexFromCode(code);
-
-                if (languageIndex < 0)
-                    Main.Error($"language {code} not currently loaded.");
-                else
-                    using (var sr = new StreamReader(filename))
-                    {
-                        String line;
-                        while ((line = sr.ReadLine()) != null)
-                        {
-                            var splitted = line.Split(new[] { '\t', ' ' }, 2);
-                            var term = splitted[0];
-                            var text = splitted[1];
-                            languageSourceData.AddTerm(term).Languages[languageIndex] = text;
-                        }
-                    }
-            }
-        }
-
         internal static bool Load(UnityModManager.ModEntry modEntry)
         {
             try
             {
                 Logger = modEntry.Logger;
-
-                LoadTranslations();
+                Translations.Load($@"{UnityModManager.modsPath}/SolastaModHelpers");
 
                 var harmony = new Harmony(modEntry.Info.Id);
                 harmony.PatchAll(Assembly.GetExecutingAssembly());
---
+++ SolastaModHelpers/Translation.cs
@@ -0,0 +1,47 @@
+﻿using I2.Loc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolastaModHelpers
+{
+    public static class Translations
+    {
+        public static void Load(string fromFolder)
+        {
+            var languageSourceData = LocalizationManager.Sources[0];
+            Main.Logger.Log($"loading translations from dir: {fromFolder}");
+            foreach (var path in Directory.EnumerateFiles(fromFolder, $"Translations-??.txt"))
+            {
+                var filename = Path.GetFileName(path);
+                var code = filename.Substring(13, 2);
+                var languageIndex = languageSourceData.GetLanguageIndexFromCode(code);
+                Main.Logger.Log($"loading translations from file: {filename}");
+                if (languageIndex < 0)
+                {
+                    Main.Error($"language {code} not currently loaded.");
+                    continue;
+                }
+
+                foreach (var line in File.ReadLines(path))
+                {
+                    try
+                    {
+                        var splitted = line.Split(new[] { '\t', ' ' }, 2);
+                        var term = splitted[0];
+                        var text = splitted[1];
+
+                        languageSourceData.AddTerm(term).Languages[languageIndex] = text;
+                    }
+                    catch
+                    {
+                        Main.Error($"invalid translation line \"{line}\".");
+                    }
+                }
+            }
+        }
+    }
+}
